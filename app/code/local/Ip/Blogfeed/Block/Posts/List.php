a4a7153be5f30e600c48fba2fa28eb477629e41a
Ryan Tulino <ryan@inverseparadox.net>
2014-06-22 07:22:44 -0400

    meta title for list page

--- app/code/local/Ip/Blogfeed/Block/Posts/List.php
+++ app/code/local/Ip/Blogfeed/Block/Posts/List.php
@@ -9,4 +9,11 @@ class Ip_Blogfeed_Block_Posts_List extends Mage_Core_Block_Template
         $this->setPostCollection(Mage::registry('blogfeed_collection'));
     }
 
+    protected function _prepareLayout()
+    {
+        if ($meta_title = Mage::getStoreConfig('blogfeed/posts/list_meta')) {
+            $this->getLayout()->getBlock('head')->setTitle($meta_title);
+        }
+    }
+
 }
